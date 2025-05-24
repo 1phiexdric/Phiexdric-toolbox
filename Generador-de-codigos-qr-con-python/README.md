b7ba66eb74d6e01ea5b052136835f0f992f52ebe
Friedrich Ruiz <ruizlopezfriedrichjavier@gmail.com>
2025-05-24 13:58:26 -0400

    adding qr generator

---
+++ Generador-de-codigos-qr-con-python/README.md
@@ -0,0 +1,47 @@
+# Generador de Códigos QR
+
+Esta es una aplicación en Python que permite generar códigos QR en formato PNG o SVG a partir de texto o URLs. Los códigos QR generados se guardan automáticamente en una carpeta específica y se ofrece la opción de abrirlos directamente desde la aplicación. Ideal para proyectos personales o educativos.
+
+## Características
+
+- Genera códigos QR en dos formatos:
+  - **PNG**: Imagen con fondo.
+  - **SVG**: Imagen vectorial sin fondo.
+- Guarda los códigos QR generados en una carpeta llamada `Qr generados`.
+- Permite personalizar el nombre del archivo generado.
+- Ofrece la opción de abrir el archivo generado automáticamente.
+
+## Requisitos
+
+- Python 3.9 o superior.
+- Librerías necesarias (instalarlas con `pip`):
+  - [qrcode](https://pypi.org/project/qrcode/)
+
+## Instalación
+
+1. Clona este repositorio o descarga los archivos.
+2. Instala las dependencias necesarias instalando el requirements o ejecutando:
+
+   ```bash
+   pip install qrcode[pil]
+
+3. Ejecuta el archivo app.py para iniciar la aplicación.
+
+## Uso
+
+Al ejecutar la aplicación, selecciona el formato del código QR que deseas generar:
+
+1.  **png** para PNG.
+2.  **svg** para SVG.
+
+Ingresa el texto o URL que deseas convertir en un código QR.
+
+Escribe el nombre que deseas darle al archivo generado (o presiona Enter para usar el nombre predeterminado `codigo_qr`).
+
+El código QR se generará y se guardará en la carpeta `Qr generados`.
+
+Decide si deseas abrir el archivo generado directamente desde la aplicación.
+
+## Licencia
+
+Este proyecto está bajo la licencia MIT. Consulta el archivo [LICENSE](LICENSE) para obtener más detalles.
---
+++ Generador-de-codigos-qr-con-python/src/app.py
@@ -0,0 +1,66 @@
+import qrcode
+import qrcode.image.svg
+import time
+import os
+import subprocess
+
+CARPETA = 'Qr generados'
+ruta_actual = os.path.dirname(os.path.abspath(__file__))#devuelve un string con la ruta
+ruta_padre = os.path.dirname(ruta_actual)#obtine la ruta padre
+destine = os.path.join(ruta_padre, CARPETA)#une la ruta con la carpeta o con la otra ruta creada
+os.makedirs(destine, exist_ok=True)#crea la carpeta si no existe, si existe no hace nada
+
+print('Generador de codigos qr')
+print('*' * 40)
+
+while True:
+    texto = input("Ingrese el texto o la url: ").strip()#strip elimina espacios que estén al principio o final del texto
+    # evalua la longitud del texto
+    if texto:#texto devuelve True si no esta vacio, lo que hace que la condicion se cumpla
+        break
+    print("El texto o la url no pueden estar vacios")
+
+nombre_qr = input('ingrese el nombre que desea darle al archivo: ')
+
+if not nombre_qr:#verifica si nombre_qr esta vacio, si lo esta se ejecuta esta condicion
+    nombre_qr = "codigo_qr"
+
+
+while True:
+    formato = input('Escribe "png" o "svg", para elegir el formato: ').lower()
+    #lower() tranforma la letra en minuscula
+    if formato in("png", "svg"):#evaluo si formato tiene como valor "png" o "svg"
+        break
+    print("Seleccion no válida")
+
+ # Creacion del codigo qr. Utilizamos la funcion .make()
+ # lo generamos a partir del valor de la variable texto
+if formato == "png":
+    img = qrcode.make(texto)
+else:
+    #formato svg
+    #agregamos la funcion
+    img = qrcode.make(texto, image_factory=qrcode.image.svg.SvgPathImage)
+
+
+print('Generado qr...')
+time.sleep(2)
+# la extension del archivo ira de acuerdo al valor de formato
+print('Generado qr...')
+ruta_qr = os.path.join(destine, f"{nombre_qr}.{formato}")
+img.save(ruta_qr)
+print('Codigo qr generado con exito')
+
+print(f'archivo guardado en la ruta actual {destine}')
+
+abrir = input('¿Desea abrir el archivo?(s/n): ')
+if abrir.lower() == 's':
+    if os.name == 'nt':
+        os.startfile(ruta_qr)
+    else:
+        try:
+            subprocess.run(['xdg-open', ruta_qr], check=True)
+        except FileNotFoundError:
+            print(f"[!] Error: No se encontró la herramienta 'xdg-open'. Asegúrate de que esté instalada.")
+        except subprocess.CalledProcessError as e:
+            print(f"[!] Error al abrir el archivo: {e}")
